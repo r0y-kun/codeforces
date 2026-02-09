07bc8cafdc2c39ce792160a20e787b9c4be30e52
r0y-kun <0roy.kun@gmail.com>
2026-02-09 23:40:35 +0530

    Update Archive

---
+++ TrippiTroppi_2094a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t; cin >> t;
+  while (t--) {
+    string s1, s2, s3; cin >> s1 >> s2 >> s3;
+
+    cout << s1[0] << s2[0] << s3[0] << endl;
+  }
+  return 0;
+}
